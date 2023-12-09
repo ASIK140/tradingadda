eb264afde27ff837992c8d141d29b3a555c81a69
Saikat <saikat2@gmail.com>
2023-12-09 20:27:32 +0530

    cors update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://tradingaddaacademy.in",
   })
 );
 app.use(bodyParser.urlencoded({ extended: false }));
