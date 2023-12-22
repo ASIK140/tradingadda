b21a1a2015e45205e0dc3ee1df741f8427bff97e
ASIK SK <140266771+ASIK140@users.noreply.github.com>
2023-12-23 04:17:08 +0530

    httpOnly False

--- src/Utils/Cookies.js
+++ src/Utils/Cookies.js
@@ -4,8 +4,8 @@ const sendCookie = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * (24 * 60 * 60 * 1000)
     ),
-    httpOnly: true,
-    sameSite: "Lax",
+    httpOnly: false,
+    sameSite: "None",
     secure: true,
   };
 
