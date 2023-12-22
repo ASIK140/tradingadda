34c81e3f6e4104891eba298c951d3406a8cb13aa
ASIK SK <140266771+ASIK140@users.noreply.github.com>
2023-12-22 23:01:30 +0530

    Update Cookies.js

--- src/Utils/Cookies.js
+++ src/Utils/Cookies.js
@@ -5,8 +5,8 @@ const sendCookie = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * (24 * 60 * 60 * 1000)
     ),
     httpOnly: true,
-    sameSite: "none",
-    secure: false,
+    sameSite: "Lax",
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
