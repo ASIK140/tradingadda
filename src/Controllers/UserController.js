809684a3ce777a85e2d7ad1eebf24f6108adc995
ASIK SK <140266771+ASIK140@users.noreply.github.com>
2023-12-23 04:33:30 +0530

    Update UserController.js

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -52,6 +52,8 @@ exports.SignOut = AsyncError(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    sameSite: "None",
+    secure: true,
   });
   res.status(200).json({
     success: true,
